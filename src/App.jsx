4b5d46a2477be191443e67bc0869016d63e04dc7
Nikhil-sys410 <n64041052@gmail.com>
2025-05-18 22:05:57 +0530

    update sidebar and app component styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import sidebar from './components/sidebar'
+import Sidebar from "./components/sidebar";
 
 const App = () => {
-  return(
-    <div className='h-screen bg-black'>
-      <div className="h-[90%] flex">
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-90% flex">
         <sidebar />
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
