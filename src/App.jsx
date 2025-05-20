7a47419c2ea53b212b49449ff5c619a6da91c1b7
Nikhil-sys410 <n64041052@gmail.com>
2025-05-20 19:08:27 +0530

    feat: add Display and DisplayHome components with Navbar integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Sidebar from "./components/sidebar";
+import Display from './components/Display'
 import Player from "./components/Player";
 
 function App() {
   return (
     <div className='h-screen bg-black'>
       <div className="h-[90%] flex">
-        
         <Sidebar />
+        <div>
+          <Display />
+        </div>
       </div>
       <Player />
     </div>
