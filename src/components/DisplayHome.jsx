103840bffe9a8625fdb7282ced83b56c1e6dd732
Nikhil-sys410 <n64041052@gmail.com>
2025-05-22 14:20:08 +0530

    feat: add AlbumItem component for displaying album details

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,11 +1,12 @@
 import Navbar from "./Navbar";
+import { albumsData } from '../assets/assets'
 
 const DisplayHome = () => {
     return (
         <>
         <Navbar />
         <div className="my-5 font-bold text-2xl">
-            
+            {albumsData.map((item,index)=>(<AlbumItem/>))}
         </div>
         </>
     );
