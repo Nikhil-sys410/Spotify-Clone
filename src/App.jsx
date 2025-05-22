9f5e48e971d7f24c8d2747170dde28784529ff8f
Nikhil-sys410 <n64041052@gmail.com>
2025-05-22 16:48:38 +0530

    feat: implement DisplayAlbum and SongItem components with player context integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import Sidebar from "./components/sidebar";
 import Display from "./components/Display";
 import Player from "./components/Player";
+import { useContext } from "react";
+import { PlayerContext } from "./context/PlayerContext.jsx";
+
 
 function App() {
+
+  const {audioRef, track} = useContext(PlayerContext)
+
+
   return (
     <div className='h-screen bg-black'>
       <div className="h-[90%] flex">
@@ -10,6 +17,7 @@ function App() {
         <Display />
       </div>
       <Player />
+      <audio ref={audioRef} src={track.file} preload="auto"></audio>
     </div>
   );
 }
